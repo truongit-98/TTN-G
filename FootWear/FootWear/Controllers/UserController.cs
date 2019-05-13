91288fc397874cc9d19cbad5048841fa0cd3bba7
phungviettung <tungxyz.11@gmail.com>
2019-05-13 22:51:01 +0700

    Giao diện cơ bản và tổ chức trong mô hình MVC

---
+++ FootWear/FootWear/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootWear.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
