1e9a268e2794fa8ddbcce41ae361eae46e2daadb
phungviettung <tungxyz.11@gmail.com>
2019-06-11 21:21:36 +0700

    ViewModel, add class SessionNamePassAd.cs và Cart.cs

---
+++ FootWear/FootWear/Models/ViewModel/SessionNamePassAd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootWear.Models.ViewModel
+{
+    public static class SessionNamePassAd
+    {
+        public static string name { get; set; }
+        public static string pass { get; set; }
+    }
+}
